78f0bbfe309033bfddd6d83b1b2abb07ce7584fa
Zach <wakuwakuzakku@gmail.com>
2023-10-27 17:32:58 +0900

    setting up to make a button

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ToastAndroid, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+  const showToast = () => {
+    console.log("Toast clicked");
+    ToastAndroid.show(
+      "You clicked this toast!",
+      ToastAndroid.SHORT,
+      ToastAndroid.BOTTOM
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>This is my first mobile app</Text>
+      <Text style={styles.text}>This is my first mobile app</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +27,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    color: "#304ffe",
+    fontSize: 24,
+    padding: 20
+    },
+
+    button: {
+      borderRadius: 10,
+      backgroundColor: "#29b6f6",
+      width: 150,
+      height: 50,
+      justifyContent: "center",
+      alignItems: "center"
+    }
 });
